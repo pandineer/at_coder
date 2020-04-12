c13beeeb61abdc0695c00dcb6b69109e310caf72
pandineer <pandineer@gmail.com>
2020-04-12 18:15:30 +0900

    runtime error

--- contests/abs/ABC049C-Hakuchumu/main.rb
+++ contests/abs/ABC049C-Hakuchumu/main.rb
@@ -1,24 +1,24 @@
-s = gets
+s = gets.chomp
 
 @answer = 'NO'
 
 def check(str)
-  if str == "\n"
+  if str == ''
     @answer = 'YES'
     return
   end
   if str.start_with?('dream')
     check(str.slice('dream'.length, str.length - 'dream'.length))
   end
-  return if @answer == 'yes'
+  return if @answer == 'YES'
   if str.start_with?('dreamer')
     check(str.slice('dreamer'.length, str.length - 'dreamer'.length))
   end
-  return if @answer == 'yes'
+  return if @answer == 'YES'
   if str.start_with?('erase')
     check(str.slice('erase'.length, str.length - 'erase'.length))
   end
-  return if @answer == 'yes'
+  return if @answer == 'YES'
   if str.start_with?('eraser')
     check(str.slice('eraser'.length, str.length - 'eraser'.length))
   end
