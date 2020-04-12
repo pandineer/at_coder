c490c77b995a48ed46eebe3af098eba9e3444b80
pandineer <pandineer@gmail.com>
2020-04-12 15:48:25 +0900

    runtime error

---
+++ contests/abs/ABC049C-Hakuchumu/main.rb
@@ -0,0 +1,29 @@
+s = gets
+
+@answer = 'NO'
+
+def check(str)
+  if str == "\n"
+    @answer = 'YES'
+    return
+  end
+  if str.start_with?('dream')
+    check(str.slice('dream'.length, str.length - 'dream'.length))
+  end
+  return if @answer == 'yes'
+  if str.start_with?('dreamer')
+    check(str.slice('dreamer'.length, str.length - 'dreamer'.length))
+  end
+  return if @answer == 'yes'
+  if str.start_with?('erase')
+    check(str.slice('erase'.length, str.length - 'erase'.length))
+  end
+  return if @answer == 'yes'
+  if str.start_with?('eraser')
+    check(str.slice('eraser'.length, str.length - 'eraser'.length))
+  end
+end
+
+check(s)
+
+puts @answer
